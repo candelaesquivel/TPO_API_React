23244eaf27582180d63184cd80d52540bbcb898e
BertiRean <tplaza15@gmail.com>
2022-04-29 21:34:52 -0300

    Mis Recetas: Agregado boton para agregar recetas.

--- foodwebpage/src/components/ProfileRecipesBody.js
+++ foodwebpage/src/components/ProfileRecipesBody.js
@@ -3,6 +3,8 @@ import SearchBar from "./SearchBar";
 import RecipeGrid from "./RecipeGrid";
 import { Box } from "@mui/system";
 import faker from "@faker-js/faker";
+import { Button } from "@mui/material";
+import { Grid } from "@mui/material";
 
 const recipeProfileExamples = [
     { 'name' : faker.lorem.word(), 'description' : faker.lorem.sentence() },
@@ -18,6 +20,20 @@ class ProfileRecipesBody extends Component{
         return (
             <>
                 <Box sx={{ flexGrow: 1 }} component='body'>
+                <Grid 
+                        container 
+                        spacing={{ xs: 2, md: 3 }} 
+                        columns={{ xs: 4, sm: 8, md: 12 }} 
+                        p={12}
+                        justifyContent='center'
+                        alignItems={'center'}
+                        direction='column'
+                >
+                    <Grid item xs={4} sm={8} md={12}>
+                        <Button variant='contained'>Agregar Nueva Receta</Button>
+                    </Grid>
+
+                </Grid>
                     <SearchBar></SearchBar>
                     <RecipeGrid 
                         recipes={recipeProfileExamples}
