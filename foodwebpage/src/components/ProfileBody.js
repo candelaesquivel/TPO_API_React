25abf443e911eea818d406090a309af3d7120a65
BertiRean <tplaza15@gmail.com>
2022-05-06 17:27:06 -0300

    Profile: Modificada pagina de perfil para solo cumplir con los requerimientos exigidos.

--- foodwebpage/src/components/ProfileBody.js
+++ foodwebpage/src/components/ProfileBody.js
@@ -4,10 +4,10 @@ import { Container } from "@mui/material";
 import { Box } from "@mui/system";
 import { Grid } from "@mui/material";
 import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
 import RestaurantIcon from '@mui/icons-material/Restaurant';
 import MessageIcon from '@mui/icons-material/Message';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { Link } from "react-router-dom";
 
 
 class ProfileBody extends Component{
@@ -25,95 +25,63 @@ class ProfileBody extends Component{
                     }}
                     >
                 </Box>
+                    <Grid container item spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                        <Grid container item xs={4} sm={8} md={12}>
+                            <Grid item xs={4} sm={8} md={12} align='center'>
+                                <AccountCircleIcon></AccountCircleIcon>
 
-               <AccountCircleIcon></AccountCircleIcon>
-                <Typography variant='h6'>
-                    Mi Perfil
-                </Typography>
-
-                
-
-                <Box component="form" noValidate sx={{ mt: 3 }} onSubmit={this.handleOnSubmit}>
-                    <Grid container spacing={2}>
-
-                        <Grid container item xs={12} spacing={2}>
-                            <Grid item>
-                            <Typography>
-                                Correo: {this.props.email}
-                            </Typography>
                             </Grid>
-                            <Grid item>
-                            <Button variant="text" xs={4} paddingBotton={6}>Modificar</Button>
+                            <Grid item xs={4} sm={8} md={12} align='center'>
+                                <Typography variant='h6'>Mi Perfil</Typography>
                             </Grid>
-
                         </Grid>
 
-                        <Grid container item xs={12} spacing={2}>
-                            <Grid item>
+                        <Grid item xs={4} sm={8} md={6}>
                             <Typography>
-                            Contrasena: {this.props.password}
+                                Nombre: {this.props.name}
                             </Typography>
-                            </Grid>
-                            <Grid item>
-                            <Button variant="text" xs={4} paddingBotton={6}>Modificar</Button>
-                            </Grid>
-
                         </Grid>
-                       
-                        <Grid container item xs={12} spacing={2}>
-                            <Grid item>
-                            <Typography>
-                            Nombre: {this.props.name}
-                            </Typography>
-                            </Grid>
-                            <Grid item>
-                            <Button variant="text" xs={4} paddingBotton={6}>Modificar</Button>
-                            </Grid>
-
-                        </Grid>
-                        
-                        <Grid container item xs={12} spacing={2}>
-                            <Grid item>
-                            <Typography>
-                            Apellido: {this.props.lastname}
-                            </Typography>
-                            </Grid>
-                            <Grid item>
-                            <Button variant="text" xs={4} paddingBotton={6}>Modificar</Button>
-                            </Grid>
-
+            
+                        <Grid item xs={4} sm={8} md={6}>
+                            <Button variant='contained' xs={4} marginTop={12}>Modificar</Button>
                         </Grid>
 
-                        <Grid container item xs={12} spacing={2}>
-                            <Grid item>
+                        <Grid item xs={4} sm={8} md={6}>
                             <Typography>
-                            Telefono: {this.props.phone}
+                                Contraseña: {this.props.password}
                             </Typography>
-                            </Grid>
-                            <Grid item>
-                            <Button variant="text" xs={4} paddingBotton={6}>Modificar</Button>
-                            </Grid>
+                        </Grid>
 
+                        <Grid item xs={4} sm={8} md={6}>
+                            <Button variant='contained' xs={4} paddingBotton={6}>Modificar</Button>
+                        </Grid>
+
+                        <Grid item xs={4} sm={8} md={6}>
+                            <Typography>
+                                Apellido: {this.props.lastname}
+                            </Typography>
+                        </Grid>
+
+                        <Grid item xs={4} sm={8} md={12}>
+                            <Typography>
+                                Email: {this.props.email}
+                            </Typography>
+                        </Grid>
+
+                        <Grid item xs={4} sm={8} md={12}>
+                            <Typography>
+                                Telefono: {this.props.phone}
+                            </Typography>
                         </Grid>
                             
                      
-                        <Grid item xs={12}>
-                        <Button fullWidth variant='contained'  startIcon={<RestaurantIcon/>} >
-                            Mis recetas
-                        </Button>
+                        <Grid item xs={4} sm={8} md={12}>
+                            <Button fullWidth variant='contained'  startIcon={<RestaurantIcon/>} component={Link} to='/my-recipes'>
+                                Mis recetas
+                            </Button>
                         </Grid>
-
-                        <Grid item xs={12} >
-                        <Button fullWidth variant='contained'  startIcon={<MessageIcon/>}>
-                          Mis comentarios
-                        </Button>
-                        </Grid>
-
 
                    </Grid>
-            
-                </Box>
-
             </Container>
     );
  }
