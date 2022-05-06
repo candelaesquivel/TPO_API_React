6e6b0edb7603f7dc690666933c6ff16769880192
BertiRean <tplaza15@gmail.com>
2022-05-06 15:03:20 -0300

    Agregado User Info array

---
+++ foodwebpage/src/utilities/sharedData.js
@@ -0,0 +1,23 @@
+const users_info = [
+    {
+        "name" : "Candela",
+        "last_name" : "Esquivel",
+        "email" : "candela@hotmail.com",
+        "phone" : "11 22 52 34 22", 
+        "password" : "1234", 
+        "question" : "¿Nombre de tu mamá?", 
+        "security_answer" : "viviana" 
+    },
+
+    {
+        "name" : "Antonio",
+        "last_name" : "Berti",
+        "email" : "antonio@hotmail.com",
+        "phone" : "11 22 40 12 34", 
+        "password" : "5678", 
+        "question" : "¿Apellido paterno?", 
+        "security_answer" : "berti" 
+    },
+];
+
+export default users_info;
