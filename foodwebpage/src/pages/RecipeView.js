1e11cdd136e011e2aa66a0daf121ca36fec13e24
BertiRean <tplaza15@gmail.com>
2022-05-07 00:50:12 -0300

    Recipe Page: Agregado parametro de id receta a la ruta

--- foodwebpage/src/pages/RecipeView.js
+++ foodwebpage/src/pages/RecipeView.js
@@ -2,17 +2,17 @@ import { Component } from "react";
 import ResponsiveFooter from "../components/ResponsiveFooter";
 import ResponsiveAppBar from "../components/ResponsiveNavBar";
 import RecipeBody from "../components/RecipeBody";
+import { useParams } from "react-router-dom";
 
-class RecipeViewPage extends Component{
-    render(){
-        return (
-            <div>
-                <ResponsiveAppBar></ResponsiveAppBar>
-                <RecipeBody></RecipeBody>
-                <ResponsiveFooter></ResponsiveFooter>
-            </div>
-        )
-    }
+export default function RecupeViewPage(props) {
+    
+    const { id } = useParams();
+
+    return (
+        <div>
+            <ResponsiveAppBar></ResponsiveAppBar>
+            <RecipeBody idRecipe={id}></RecipeBody>
+            <ResponsiveFooter></ResponsiveFooter>
+        </div>
+    )
 }
-
-export default RecipeViewPage;
