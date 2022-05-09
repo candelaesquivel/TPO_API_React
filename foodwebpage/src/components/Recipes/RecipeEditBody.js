c232779541f3321dcfad480502a3222293e23e52
BertiRean <tplaza15@gmail.com>
2022-05-09 00:00:06 -0300

    Recetas: Corregido Warning

--- foodwebpage/src/components/Recipes/RecipeEditBody.js
+++ foodwebpage/src/components/Recipes/RecipeEditBody.js
@@ -1,4 +1,4 @@
-import { Button, Grid, Stack, Typography } from "@mui/material";
+import { Grid, Stack, Typography } from "@mui/material";
 import { TextField } from "@mui/material";
 import { FormGroup } from "@mui/material";
 import { Switch } from "@mui/material";
