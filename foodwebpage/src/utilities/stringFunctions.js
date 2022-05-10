34b48179af1ae2f2b783dd392cd4290409bf01d5
BertiRean <tplaza15@gmail.com>
2022-05-10 16:02:51 -0300

    StringFunctions: Agregadas funciones para validacion de email y password.

--- foodwebpage/src/utilities/stringFunctions.js
+++ foodwebpage/src/utilities/stringFunctions.js
@@ -1,10 +1,38 @@
 // Ask the Time Complexity of startsWith in terms of O(n)
 export function prefixStr(source, target){
-    if (target.lenght === 0)
+    if (target.length === 0)
         return true;
 
     const srcLower = source.toLowerCase();
     const targetLower = target.toLowerCase();
     
     return srcLower.startsWith(targetLower);
+}
+
+export function isNumeric(str){
+    return /\d/.test(str)
+}
+
+export function isValidPassword(password){
+
+    if (password.length === 0)
+        return true;
+
+    if (password.length < 7)
+        return false;
+
+    const validPassword = new RegExp('^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$');
+    return validPassword.test(password);
+}
+
+export function isValidEmailWithRegex(email){
+
+    if (email.length === 0)
+        return true;
+
+    const validEmail = new RegExp(
+        '^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$'
+     );
+
+    return validEmail.test(email);
 }
