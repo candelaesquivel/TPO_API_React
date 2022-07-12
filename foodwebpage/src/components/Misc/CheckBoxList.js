fff726a4241adae3e7289c1f1c75c011ea959bef
BertiRean <tplaza15@gmail.com>
2022-07-12 19:23:59 -0300

    CheckBoxList: Agregado codigo para mostrar items marcados cuando se recibe una lista de marcados

--- foodwebpage/src/components/Misc/CheckBoxList.js
+++ foodwebpage/src/components/Misc/CheckBoxList.js
@@ -4,22 +4,30 @@ import { useState } from "react";
 
 export function CheckBoxList(props)
 {
+    const hasSelected = props.markedItems !== undefined
+    const markedItems = props.markedItems
+    console.log("Props: ", hasSelected)
     return (
         <List sx={{ width: 'auto', maxWidth: 360, bgcolor: 'background.paper', 
         overflow : props.overflow , maxHeight : props.maxHeight }}>
         {
             props.items.map((itr, index) => 
+            {
+                const checked = hasSelected && markedItems.includes(itr)
+                return (
                 <ListItem>
                     <ListItemIcon>
                         <Checkbox
                             id = {index + props.category_col} 
                             onChange= {props.onChecked}
+                            defaultChecked={checked}
                         >
                         </Checkbox>
                     </ListItemIcon>
                     <ListItemText>{itr}</ListItemText>
                 </ListItem>
-            )
+                )
+            })
         }
         </List>
     )
