8569131315517e15004189a219e180c23c3ae9fb
BertiRean <tplaza15@gmail.com>
2022-07-12 22:26:23 -0300

    MessagePageView: Disminuido tiempo de espera

--- foodwebpage/src/pages/MessagePageView.js
+++ foodwebpage/src/pages/MessagePageView.js
@@ -21,7 +21,7 @@ export default function MessagePageView(props){
         else{
             setTimeout( () => {
                 navigate(props.url)
-            }, 2000);
+            }, 1000);
         }
     });
 
